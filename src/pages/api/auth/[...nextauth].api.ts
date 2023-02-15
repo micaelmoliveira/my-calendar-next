975645be9870dc3fb418726568d9315257cb2c2c
Micael <micaelmoura.oliveira@gmail.com>
2023-02-15 19:29:00 -0300

    callback session nexth auth

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,47 +1,58 @@
-import { PrismaAdapter } from "@/lib/auth/prisma-adapter"
-import { NextApiRequest, NextApiResponse } from "next"
-import { NextAuthOptions } from "next-auth"
-import NextAuth from "next-auth/next"
-import GoogleProvider, { GoogleProfile } from "next-auth/providers/google"
+import { PrismaAdapter } from "@/lib/auth/prisma-adapter";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 
-export function buildAuthOptions(req : NextApiRequest, res: NextApiResponse): NextAuthOptions {
+export function buildAuthOptions(
+  req: NextApiRequest,
+  res: NextApiResponse
+): NextAuthOptions {
   return {
     adapter: PrismaAdapter(req, res),
-  
+
     providers: [
       GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
         authorization: {
           params: {
-            scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar'
-          }
+            scope:
+              "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar",
+          },
         },
         profile(profile: GoogleProfile) {
           return {
             id: profile.sub,
             name: profile.name,
-            username: '',
+            username: "",
             email: profile.email,
             avatar_url: profile.picture,
-          }
-        }
+          };
+        },
       }),
     ],
-  
+
     callbacks: {
       async signIn({ account }) {
-        if (!account?.scope?.includes('https://www.googleapis.com/auth/calendar')) {
-          return '/register/calendar/?error=permissions'
+        if (
+          !account?.scope?.includes("https://www.googleapis.com/auth/calendar")
+        ) {
+          return "/register/calendar/?error=permissions";
         }
-  
-        return true
+        return true;
+      },
+
+      async session({ session, user }) {
+        return {
+          ...session,
+          user
+        }
       }
-    }
-  }
-  
+    },
+  };
 }
 
-export default async function auth(req : NextApiRequest, res: NextApiResponse) {
-  return await NextAuth(req, res, buildAuthOptions(req, res))
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildAuthOptions(req, res));
 }
