640320557d59465752761e3a65ecd99efcbe2683
Micael <micaelmoura.oliveira@gmail.com>
2023-02-17 08:56:26 -0300

    Checkbox com Controller do react hook form

--- src/pages/register/calendar-availability/index.page.tsx
+++ src/pages/register/calendar-availability/index.page.tsx
@@ -8,7 +8,7 @@ import {
   TextInput,
 } from "@ignite-ui/react";
 import { ArrowRight } from "phosphor-react";
-import { useFieldArray, useForm } from "react-hook-form";
+import { Controller, useFieldArray, useForm } from "react-hook-form";
 import { z } from "zod";
 import { Container, Header } from "../styles";
 import {
@@ -26,17 +26,18 @@ export default function CalendarAvailability() {
     register,
     handleSubmit,
     control,
+    watch,
     formState: { isSubmitting, errors },
   } = useForm({
     defaultValues: {
       availability: [
-        { weekday: 0, enable: false, startTime: "08:00", endTime: "18:00" },
-        { weekday: 1, enable: true, startTime: "08:00", endTime: "18:00" },
-        { weekday: 2, enable: true, startTime: "08:00", endTime: "18:00" },
-        { weekday: 3, enable: true, startTime: "08:00", endTime: "18:00" },
-        { weekday: 4, enable: true, startTime: "08:00", endTime: "18:00" },
-        { weekday: 5, enable: true, startTime: "08:00", endTime: "18:00" },
-        { weekday: 6, enable: false, startTime: "08:00", endTime: "18:00" },
+        { weekday: 0, enabled: false, startTime: "08:00", endTime: "18:00" },
+        { weekday: 1, enabled: true, startTime: "08:00", endTime: "18:00" },
+        { weekday: 2, enabled: true, startTime: "08:00", endTime: "18:00" },
+        { weekday: 3, enabled: true, startTime: "08:00", endTime: "18:00" },
+        { weekday: 4, enabled: true, startTime: "08:00", endTime: "18:00" },
+        { weekday: 5, enabled: true, startTime: "08:00", endTime: "18:00" },
+        { weekday: 6, enabled: false, startTime: "08:00", endTime: "18:00" },
       ],
     },
   });
@@ -47,6 +48,8 @@ export default function CalendarAvailability() {
     control,
     name: "availability",
   });
+
+  const availability = watch("availability");
 
   async function handleSetCalendarAvailability() {}
 
@@ -71,12 +74,37 @@ export default function CalendarAvailability() {
             return (
               <CalendarAvailabilityItem key={field.id}>
                 <CalendarAvailabilityDay>
-                  <Checkbox />
+                  <Controller
+                    name={`availability.${index}.enabled`}
+                    control={control}
+                    render={({ field }) => {
+                      return (
+                        <Checkbox
+                          onCheckedChange={(checked) => {
+                            field.onChange(checked === true);
+                          }}
+                          checked={field.value}
+                        />
+                      );
+                    }}
+                  />
                   <Text>{weekDays[field.weekday]}</Text>
                 </CalendarAvailabilityDay>
                 <CalendarAvailabilityTime>
-                  <TextInput size="sm" type="time" step="60" {...register(`availability.${index}.startTime`)} />
-                  <TextInput size="sm" type="time" step="60" {...register(`availability.${index}.endTime`)} />
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step="60"
+                    disabled={availability[index].enabled === false}
+                    {...register(`availability.${index}.startTime`)}
+                  />
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step="60"
+                    disabled={availability[index].enabled === false}
+                    {...register(`availability.${index}.endTime`)}
+                  />
                 </CalendarAvailabilityTime>
               </CalendarAvailabilityItem>
             );
