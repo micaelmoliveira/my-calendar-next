16ae0ed8a67f1bd09dbd45ea326cf15365c5f685
Micael <micaelmoura.oliveira@gmail.com>
2023-02-16 21:59:32 -0300

    Formulário de disponibilidade

--- src/pages/register/calendar-availability/index.page.tsx
+++ src/pages/register/calendar-availability/index.page.tsx
@@ -1,5 +1,15 @@
-import { Button, Checkbox, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
+import { getWeekDays } from "@/utils/get-week-days";
+import {
+  Button,
+  Checkbox,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from "@ignite-ui/react";
 import { ArrowRight } from "phosphor-react";
+import { useFieldArray, useForm } from "react-hook-form";
+import { z } from "zod";
 import { Container, Header } from "../styles";
 import {
   CalendarAvailabilityBox,
@@ -9,7 +19,37 @@ import {
   CalendarAvailabilityTime,
 } from "./styles";
 
+const CalendarAvailabilityFormSchema = z.object({});
+
 export default function CalendarAvailability() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    defaultValues: {
+      availability: [
+        { weekday: 0, enable: false, startTime: "08:00", endTime: "18:00" },
+        { weekday: 1, enable: true, startTime: "08:00", endTime: "18:00" },
+        { weekday: 2, enable: true, startTime: "08:00", endTime: "18:00" },
+        { weekday: 3, enable: true, startTime: "08:00", endTime: "18:00" },
+        { weekday: 4, enable: true, startTime: "08:00", endTime: "18:00" },
+        { weekday: 5, enable: true, startTime: "08:00", endTime: "18:00" },
+        { weekday: 6, enable: false, startTime: "08:00", endTime: "18:00" },
+      ],
+    },
+  });
+
+  const weekDays = getWeekDays();
+
+  const { fields } = useFieldArray({
+    control,
+    name: "availability",
+  });
+
+  async function handleSetCalendarAvailability() {}
+
   return (
     <Container>
       <Header>
@@ -22,44 +62,25 @@ export default function CalendarAvailability() {
         <MultiStep size={4} currentStep={3} />
       </Header>
 
-      <CalendarAvailabilityBox as="form">
+      <CalendarAvailabilityBox
+        as="form"
+        onSubmit={handleSubmit(handleSetCalendarAvailability)}
+      >
         <CalendarAvailabilityContainer>
-          <CalendarAvailabilityItem>
-            <CalendarAvailabilityDay>
-              <Checkbox />
-              <Text>Segunda-feira</Text>
-            </CalendarAvailabilityDay>
-            <CalendarAvailabilityTime>
-              <TextInput 
-                size="sm"
-                type="time"
-                step="60"
-              />
-              <TextInput 
-                size="sm"
-                type="time"
-                step="60"
-              />
-            </CalendarAvailabilityTime>
-          </CalendarAvailabilityItem>
-          <CalendarAvailabilityItem>
-            <CalendarAvailabilityDay>
-              <Checkbox />
-              <Text>Terça-feira</Text>
-            </CalendarAvailabilityDay>
-            <CalendarAvailabilityTime>
-              <TextInput 
-                size="sm"
-                type="time"
-                step="60"
-              />
-              <TextInput 
-                size="sm"
-                type="time"
-                step="60"
-              />
-            </CalendarAvailabilityTime>
-          </CalendarAvailabilityItem>
+          {fields.map((field, index) => {
+            return (
+              <CalendarAvailabilityItem key={field.id}>
+                <CalendarAvailabilityDay>
+                  <Checkbox />
+                  <Text>{weekDays[field.weekday]}</Text>
+                </CalendarAvailabilityDay>
+                <CalendarAvailabilityTime>
+                  <TextInput size="sm" type="time" step="60" {...register(`availability.${index}.startTime`)} />
+                  <TextInput size="sm" type="time" step="60" {...register(`availability.${index}.endTime`)} />
+                </CalendarAvailabilityTime>
+              </CalendarAvailabilityItem>
+            );
+          })}
         </CalendarAvailabilityContainer>
 
         <Button type="submit">
